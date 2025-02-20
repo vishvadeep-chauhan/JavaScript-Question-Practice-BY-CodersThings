bed7ef2871c48f63c74a4c1fc8d8d97826c5e789
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-02-20 17:24:07 +0530

    setTimeout();question.basic

--- 05. setTimeout and setInterval/0.3Assignment.js
+++ 05. setTimeout and setInterval/0.3Assignment.js
@@ -1,3 +1,10 @@
 // Q1: Delayed Greeting
 
 // Create a function called delayedHello that prints "Hello, welcome to JavaScript!" after 2 seconds using setTimeout.
+
+function delayedHello() {
+  setTimeout(() => {
+    console.log("Hello, welcome to JavaScript!");
+  }, 2000);
+}
+delayedHello();
